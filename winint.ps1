dfce7a26b37fe97917725fc9752685c4d9acb77e
simulasi2 <87004325+simulasi2@users.noreply.github.com>
2021-09-05 20:32:09 +0700

    Update winint.ps1

--- winint.ps1
+++ winint.ps1
@@ -4,6 +4,6 @@ Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd6
 tar xf ngrok.zip
 Copy ngrok.exe C:\Windows\System32
 cmd /c echo ./ngrok.exe authtoken "1uTirkBjQTOtm7pwu89SXwWq8sw_3Q4ofsdT5rhULVKhB5gJUE" >a.ps1
-cmd /c echo cmd /k start ./ngrok.exe tcp 3389 >>a.ps1
+cmd /c echo ./ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 1.1.1.1 >>a.ps1
 .\a.ps1
