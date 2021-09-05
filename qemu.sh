4d4f5fa0cdc3c6f160998a4e2c17035d43862a16
simulasi2 <87004325+simulasi2@users.noreply.github.com>
2021-09-05 18:05:14 +0700

    Update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -3,7 +3,7 @@ sudo apt install --assume-yes wget tasksel
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive
-apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo systemctl disable lightdm.service
 sudo echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
